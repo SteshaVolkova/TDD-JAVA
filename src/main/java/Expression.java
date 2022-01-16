60a1a39bb79ff05b97f1807a43a56e9bf7f2e28e
SteshaVolkova <stepashechka5@gmail.com>
2022-01-16 19:21:35 +0300

    add my TDD book test examples

---
+++ src/main/java/Expression.java
@@ -0,0 +1,9 @@
+package main.java;
+
+interface Expression {
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
+}
---
+++ src/main/java/Money.java
@@ -0,0 +1,51 @@
+package main.java;
+
+import org.junit.Test;
+
+import java.util.Objects;
+
+class Money implements Expression {
+    protected int amount;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    protected String currency;
+    String currency() {
+        return currency;
+    }
+
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && currency().equals(money.currency());
+    }
+
+    public String toString() {
+        return amount + "" + currency;
+    }
+
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount/rate, to);
+    }
+}
